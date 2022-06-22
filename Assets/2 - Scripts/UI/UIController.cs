10cd4a418006c5ea486ffe273517c8f94c9b373c
Dennis Hawran <dennishawran@gmail.com>
2022-06-22 02:16:04 +0200

    0.0.3

--- Assets/2 - Scripts/UI/UIController.cs
+++ Assets/2 - Scripts/UI/UIController.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using HighscorePlugin;
+
 public class UIController : MonoBehaviour
 {
+    [SerializeField] private Highscores highscores;
     [SerializeField] private GameObject gameOverPanel;
     [SerializeField] private GameObject gamePanel;
     [SerializeField] TMP_Text gameScoreText;
     [SerializeField] TMP_Text endScoreText;
+    [SerializeField] List<TMP_Text> highscoreTexts = new List<TMP_Text>();
+    [SerializeField] List<TMP_Text> highscoreValues = new List<TMP_Text>();
+    [SerializeField] TMP_InputField nameInput;
     [SerializeField] GameController gameController;
+
+    bool scoreSubmitted = false;
+
     private void Start() {
         gameController.OnGameOver += GameOver;
     }
@@ -17,6 +26,24 @@ public class UIController : MonoBehaviour
         endScoreText.text = ((int)(gameController.Samurai.Score)).ToString();
         gameOverPanel.SetActive(true);
         gamePanel.SetActive(false);
+        
+        highscores.OnHighscoresReceived += UpdateHighscores;
+        highscores.GetHighscores(highscoreTexts.Count);
+    }
+
+    public void UpdateHighscores(List<SingleNameScore> _highscores) {
+        for (int i = 0; i < highscoreTexts.Count; i++) {
+            highscoreTexts[i].text = _highscores[i].name;
+            highscoreValues[i].text = _highscores[i].score.ToString();
+        }
+    }
+
+    public void SubmitScore() {
+        if(scoreSubmitted) return;
+
+        scoreSubmitted = true;
+        highscores.CreateHighscore(nameInput.text, (int)gameController.Samurai.Score);
+        highscores.GetHighscores(highscoreTexts.Count);
     }
 
     void Update()
